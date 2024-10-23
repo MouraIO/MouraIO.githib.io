1d84024cecfc6e4aa1a4eed30435f1645270eee6
MouraSS <murilop387@gmail.com>
2024-10-23 12:05:57 -0300

    correcao

---
+++ scr.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function () {
+  const mobileNav = document.getElementById("mobile-nav");
+  mobileNav.classList.toggle("active");
+});
